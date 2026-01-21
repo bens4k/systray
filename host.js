0e18f85af375751f5d2fc83dc9736d23d8bd259a
ab <ab_ben@live.com>
2026-01-21 16:20:59 +0000

    bug fixes

--- host.js
+++ host.js
@@ -2,7 +2,6 @@ import St from 'gi://St';
 import GObject from 'gi://GObject';
 import GLib from 'gi://GLib';
 import Clutter from 'gi://Clutter';
-import Meta from 'gi://Meta';
 
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
@@ -12,6 +11,10 @@ export const PopupGridHost = GObject.registerClass(
       super._init(0.0, 'SysTray');
       this._settings = settings;
       this._buttons = [];
+      this._idleSyncId = 0;
+
+      this.menu.actor.add_style_class_name('systray-popup');
+      this.menu.box.add_style_class_name('systray-popup-content');
 
       this._panelIcon = new St.Icon({
         icon_name: 'pan-up-symbolic',
@@ -22,7 +25,14 @@ export const PopupGridHost = GObject.registerClass(
       this._openStateId = this.menu.connect('open-state-changed', (_m, isOpen) => {
         this._panelIcon.icon_name = isOpen ? 'pan-down-symbolic' : 'pan-up-symbolic';
 
-        GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+        if (this._idleSyncId)
+          GLib.source_remove(this._idleSyncId);
+
+        this._idleSyncId = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+          this._idleSyncId = 0;
+          if (!this.get_parent()) // already destroyed/removed
+            return GLib.SOURCE_REMOVE;
+
           this._syncPopupSize();
           return GLib.SOURCE_REMOVE;
         });
@@ -37,7 +47,6 @@ export const PopupGridHost = GObject.registerClass(
       });
 
       this.menu.box.add_child(this._rowsBox);
-      this.menu.actor.set_style(`min-width:0px;`);
 
       // setting updates
       this._settingsChangedId = this._settings.connect('changed', () => {
@@ -45,7 +54,14 @@ export const PopupGridHost = GObject.registerClass(
         this._applyCellSizeToAllButtons();
         this._rebuildRows();
 
-        GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+        if (this._idleSyncId)
+          GLib.source_remove(this._idleSyncId);
+
+        this._idleSyncId = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+          this._idleSyncId = 0;
+          if (!this.get_parent()) // already destroyed/removed
+            return GLib.SOURCE_REMOVE;
+
           this._syncPopupSize();
           return GLib.SOURCE_REMOVE;
         });
@@ -56,6 +72,10 @@ export const PopupGridHost = GObject.registerClass(
     }
 
     destroy() {
+      if (this._idleSyncId) {
+        GLib.source_remove(this._idleSyncId);
+        this._idleSyncId = 0;
+      }
       if (this._openStateId) {
         try { this.menu.disconnect(this._openStateId); } catch { }
         this._openStateId = 0;
@@ -64,15 +84,14 @@ export const PopupGridHost = GObject.registerClass(
         try { this._settings.disconnect(this._settingsChangedId); } catch { }
         this._settingsChangedId = 0;
       }
-
       super.destroy();
     }
 
     _readLayoutSettings() {
-      this._maxCols = this._settings.get_int('max-columns');
-      this._cell = this._settings.get_int('cell-size');
-      this._pad = this._settings.get_int('popup-padding');
-      this._spacing = this._settings.get_int('spacing');
+      this._maxCols = Math.max(1, this._settings.get_int('max-columns'));
+      this._cell = Math.max(1, this._settings.get_int('cell-size'));
+      this._pad = Math.max(0, this._settings.get_int('popup-padding'));
+      this._spacing = Math.max(0, this._settings.get_int('spacing'));
     }
 
     _applyCellSizeToAllButtons() {
@@ -114,53 +133,11 @@ export const PopupGridHost = GObject.registerClass(
       this._syncPopupSize();
     }
 
-    _syncPopupWidthToContent() {
-      if (!this.menu.isOpen || !this.menu.actor.mapped)
-        return;
-
-      this._rowsBox.set_width(-1);
-      this.menu.box.set_width(-1);
-      this.menu.actor.set_width(-1);
-
-      this._rowsBox.queue_relayout();
-      this.menu.box.queue_relayout();
-      this.menu.actor.queue_relayout();
-
-      let [, natW] = this.menu.actor.get_preferred_width(-1);
-
-      let borderW = 0;
-      try {
-        const node = this.menu.box.get_theme_node();
-        borderW =
-          node.get_length('border-left-width') +
-          node.get_length('border-right-width');
-      } catch { }
-
-      if (natW <= 0) {
-        const n = this._buttons.length;
-        const cols = Math.max(1, Math.min(this._maxCols, n));
-        const cell = Math.max(1, this._cell);
-        const spacing = Math.max(0, this._spacing);
-        const pad = Math.max(0, this._pad);
-        natW = cols * cell + (cols - 1) * spacing + pad * 2 + borderW;
-      } else {
-        natW = natW + borderW;
-      }
-
-      const targetW = Math.ceil(natW) + 2;
-
-      this.menu.actor.set_width(targetW);
-      this.menu.box.set_width(targetW);
-      this.menu.actor.queue_relayout();
-    }
-
     _syncPopupSize() {
       const spacing = Math.max(0, this._spacing);
       const pad = Math.max(0, this._pad);
 
-      this.menu.actor.set_style('min-width: 0px;');
-      this.menu.box.set_style(`padding:${pad}px; min-width: 0px;`);
-
+      this.menu.box.set_style(`padding:${pad}px;`);
       this._rowsBox.set_style(`spacing:${spacing}px;`);
 
       this.menu.actor.x_expand = false;
@@ -169,11 +146,6 @@ export const PopupGridHost = GObject.registerClass(
 
       this.menu.box.x_align = Clutter.ActorAlign.START;
       this._rowsBox.x_align = Clutter.ActorAlign.START;
-
-      global.compositor.get_laters().add(Meta.LaterType.BEFORE_REDRAW, () => {
-        this._syncPopupWidthToContent();
-        return GLib.SOURCE_REMOVE;
-      });
     }
 
     addButton(button) {
